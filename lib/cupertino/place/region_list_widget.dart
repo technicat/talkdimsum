864fe0788cefb6b6ea8c3e97f0f0b24764970a9e
Philip Chu <atlassian@technicat.com>
2020-12-28 20:11:09 -0800

    Update region_list_widget.dart

--- lib/cupertino/place/region_list_widget.dart
+++ lib/cupertino/place/region_list_widget.dart
@@ -7,32 +7,30 @@ import 'package:talkdimsum/core/country.dart';
 
 import 'place_list_widget.dart';
 
-
 class RegionListWidget extends StatelessWidget {
-
- final Country country;
+  final Country country;
 
   RegionListWidget({Key key, @required this.country}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: ListView(
-      children:
-          country.regions.map((place) => _placeTile(context, place)).toList()));
-          }
+    return Center(
+        child: ListView(
+            children: country.regions
+                .map((place) => _placeTile(context, place))
+                .toList()));
+  }
 
- Widget _placeTile(BuildContext context, Region region) => 
- GestureDetector(
-   onTap: () {
-   Navigator.push(
-                    context,
-                    CupertinoPageRoute(
-                        builder: (context) => PlaceListWidget(region: region)),
-                  );
-   },
-   child: Text(region.name,
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 20
-            )));
+  Widget _placeTile(BuildContext context, Region region) => GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          CupertinoPageRoute(
+              builder: (context) => PlaceListWidget(region: region)),
+        );
+      },
+      child: Padding(
+          padding: EdgeInsets.all(4.0),
+          child: Text(region.name,
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20))));
 }
