72495639cdf5a7b929b007b095385fea78b66595
Philip Chu <atlassian@technicat.com>
2020-12-29 10:21:01 -0800

    Update scaffold_country_widget.dart

--- lib/cupertino/place/scaffold_country_widget.dart
+++ lib/cupertino/place/scaffold_country_widget.dart
@@ -7,55 +7,12 @@ import 'package:talkdimsum/core/country.dart';
 import 'region_list_widget.dart';
 
 class ScaffoldCountryWidget extends StatelessWidget {
-
-  Country country = Country.countries[0];
-
   @override
   Widget build(BuildContext context) {
-            return CupertinoPageScaffold(
-              navigationBar: CupertinoNavigationBar(
-                middle: Text('Places'),
-              ),
-              child: RegionListWidget(country:country)
-                            
-                          );
-                        }
-          }
-/*
-class ScaffoldCountryWidget extends StatefulWidget {
-  
-  Country country = Country.countries[0];
-
-@override
-  ScaffoldCountryState createState() => ScaffoldCountryState();
-}
-
-class ScaffoldCountryState extends State<ScaffoldCountryWidget> {
-
-  @override
-  Widget build(BuildContext context) {
-    return
-       Scaffold(
-        appBar: 
-          AppBar(
-            title: Text(widget.country.name),
-             actions: <Widget>[
-                PopupMenuButton<Country>(
-                    icon: Icon(Icons.public),
-                    onSelected: (value) {
-                      setState(() { widget.country = value; });
-                    },
-                    itemBuilder: (BuildContext context) => 
-                        Country.countries.map((country) => PopupMenuItem<Country>(value: country, child: Text(country.name))).toList(),
-
-                ),
-          ]
-              ),
-        body: RegionListWidget(country: widget.country),
-       
-    );
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text('Places'),
+        ),
+        child: RegionListWidget(country: Country.countries[0]));
   }
-
 }
-
-*/
