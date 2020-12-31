20a0c6737d143771026744e62a6179ac7fb43a6a
Philip Chu <atlassian@technicat.com>
2020-12-30 17:20:38 -0800

    Update region_list_widget.dart

--- lib/ui/material/place/region_list_widget.dart
+++ lib/ui/material/place/region_list_widget.dart
@@ -7,19 +7,19 @@ import 'package:talkdimsum/core/country.dart';
 
 import 'place_list_widget.dart';
 
-
 class RegionListWidget extends StatelessWidget {
-
- final Country country;
+  final Country country;
 
   RegionListWidget({Key key, @required this.country}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: ListView(
-      children:
-          country.regions.map((place) => _placeTile(context, place)).toList()));
-          }
+    return Center(
+        child: ListView(
+            children: country.regions
+                .map((place) => _placeTile(context, place))
+                .toList()));
+  }
 
   ListTile _placeTile(BuildContext context, Region region) => ListTile(
         title: Text(region.name,
@@ -27,17 +27,17 @@ class RegionListWidget extends StatelessWidget {
               fontWeight: FontWeight.w500,
               fontSize: 20,
             )),
-            onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => PlaceListWidget(region: region)),
-                  );
-                },
-          subtitle: region.places.length != 1 ? 
-            Text('${region.places.length} restaurants') :
-            Text('${region.places.length} restaurant'),
-       /* leading: Icon(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => PlaceListWidget(region: region)),
+          );
+        },
+        subtitle: region.places.length != 1
+            ? Text('${region.places.length} restaurants')
+            : Text('${region.places.length} restaurant'),
+        /* leading: Icon(
           icon,
           color: Colors.blue[500],
         ), */
