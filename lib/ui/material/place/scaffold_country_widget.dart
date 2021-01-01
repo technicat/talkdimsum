8bbe580746108e26810f492c645406f669e44f1c
Philip Chu <atlassian@technicat.com>
2020-12-31 20:15:30 -0800

    pinterest

--- lib/ui/material/place/scaffold_country_widget.dart
+++ lib/ui/material/place/scaffold_country_widget.dart
@@ -7,38 +7,31 @@ import 'package:talkdimsum/core/country.dart';
 import 'region_list_widget.dart';
 
 class ScaffoldCountryWidget extends StatefulWidget {
-
-@override
+  @override
   ScaffoldCountryState createState() => ScaffoldCountryState();
 }
 
 class ScaffoldCountryState extends State<ScaffoldCountryWidget> {
-
-   Country country = Country.countries[0];
+  Country country = Country.countries[0];
 
   @override
   Widget build(BuildContext context) {
-    return
-       Scaffold(
-        appBar: 
-          AppBar(
-            title: Text(country.name),
-             actions: <Widget>[
-                PopupMenuButton<Country>(
-                    icon: Icon(Icons.public),
-                    onSelected: (value) {
-                      setState(() { country = value; });
-                    },
-                    itemBuilder: (BuildContext context) => 
-                        Country.countries.map((country) => PopupMenuItem<Country>(value: country, child: Text(country.name))).toList(),
-
-                ),
-          ]
-              ),
-        body: RegionListWidget(country: country),
-       
+    return Scaffold(
+      appBar: AppBar(title: Text(country.name), actions: <Widget>[
+        PopupMenuButton<Country>(
+          icon: Icon(Icons.public),
+          onSelected: (value) {
+            setState(() {
+              country = value;
+            });
+          },
+          itemBuilder: (BuildContext context) => Country.countries
+              .map((country) => PopupMenuItem<Country>(
+                  value: country, child: Text(country.name)))
+              .toList(),
+        ),
+      ]),
+      body: RegionListWidget(country: country),
     );
   }
-
 }
-
