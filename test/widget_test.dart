81e8a21893eb88f28ebedb8d734a4dddf860a2ca
Philip Chu <atlassian@technicat.com>
2021-01-03 11:36:32 -0800

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:talkdimsum/main.dart';
+import 'package:talkdimsum/ui/myapp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
