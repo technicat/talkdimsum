ceebfa35a82404a2c292fdc722329993c810d485
Philip Chu <atlassian@technicat.com>
2021-01-04 18:41:43 -0800

    rm myapp

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,52 @@
 // directory structure is similar to
 // https://github.com/jgrandchavin/flutter_provider_architecture_starter
 
+
+import 'dart:io' show Platform;
+
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
+import 'package:talkdimsum/core/provider/dimsum.dart';
+//import 'package:talkdimsum/core/provider/favorites.dart';
+import 'package:talkdimsum/core/provider/conversation.dart';
+import 'package:talkdimsum/core/provider/countries.dart';
+
+import 'ui/cupertino/app.dart' as cupertino;
+import 'ui/material/app.dart' as material;
+
 import 'package:flutter/widgets.dart' show runApp;
 
-import 'ui/myapp.dart';
+//import 'ui/myapp.dart';
 
 void main() async {
-  // needed for accessing rootBundle for JSON
- // WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  runApp(MainApp());
 }
+
+// wrapper around the native (cupertino or material) versions
+// set up change notifier providers here for app state
+// https://flutter.dev/docs/development/data-and-backend/state-mgmt/simple
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget app;
+    try {
+      if (Platform.isIOS || Platform.isMacOS) {
+        app = cupertino.App();
+      } else {
+        app = material.App();
+      }
+    } catch (e) {
+      // Platform check above breaks on web
+      app = material.App();
+    }
+    return MultiProvider(providers: [
+      ChangeNotifierProvider<DimSum>(create: (context) => DimSum()),
+     // ChangeNotifierProvider<Favorites>(create: (context) => Favorites()),
+      ChangeNotifierProvider<Countries>(create: (context) => Countries()),
+      ChangeNotifierProvider<Conversation>(create: (context) => Conversation())
+    ], child: app);
+  }
+}
+
