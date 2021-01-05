3eb8b8d526a144ecf966af417ec9ae35aee2a2a5
Philip Chu <atlassian@technicat.com>
2021-01-04 19:50:07 -0800

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:talkdimsum/ui/myapp.dart';
+import 'package:talkdimsum/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MainApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
