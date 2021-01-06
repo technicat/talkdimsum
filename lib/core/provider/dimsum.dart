f64a760c938f6c5d9135e916bd2f146ac438ee44
Philip Chu <atlassian@technicat.com>
2021-01-06 10:47:49 -0800

    Update dimsum.dart

--- lib/core/provider/dimsum.dart
+++ lib/core/provider/dimsum.dart
@@ -3,6 +3,9 @@ import 'dart:convert'; // json
 import 'package:flutter/foundation.dart' show ChangeNotifier;
 import 'package:flutter/services.dart' show rootBundle;
 
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
 import 'package:talkdimsum/core/model/dish.dart';
 import 'package:talkdimsum/core/model/tags.dart';
 import 'package:talkdimsum/core/model/word.dart';
@@ -13,21 +16,16 @@ class DimSum with ChangeNotifier {
 
   List<Dish> favorites = [];
 
-  bool isFavorite(Dish dish) {
-    return favorites.contains(dish);
-  }
-
-  addFavorite(Dish dish) {
-    favorites.add(dish);
-    notifyListeners();
-  }
-
-  removeFavorite(Dish dish) {
-    favorites.remove(dish);
-    notifyListeners();
-  }
-
   List<String> _categories = [];
+
+  DimSum() {
+    _loadDishes();
+    _loadCategories();
+    _loadFavorites();
+    Tags.load();
+  }
+
+ 
 
   _loadDishes() async {
     var dishfiles = await rootBundle
@@ -65,9 +63,69 @@ class DimSum with ChangeNotifier {
     return tags;
   }
 
-  DimSum() {
-    _loadDishes();
-    _loadCategories();
-    Tags.load();
+   static const table = 'favorites';
+
+  static Future<Database> database() async {
+    return openDatabase(
+      // Set the path to the database. Note: Using the `join` function from the
+      // `path` package is best practice to ensure the path is correctly
+      // constructed for each platform.
+      join(await getDatabasesPath(), 'favorites.db'),
+
+      onCreate: (db, version) {
+        // Run the CREATE TABLE statement on the database.
+        return db.execute(
+          "CREATE TABLE favorites(name TEXT PRIMARY KEY)",
+        );
+      },
+      // Set the version. This executes the onCreate function and provides a
+      // path to perform database upgrades and downgrades.
+      version: 1,
+    );
+  }
+
+   bool isFavorite(Dish dish) {
+    return favorites.contains(dish);
+  }
+
+  
+/*
+  addFavorite(Dish dish) {
+    favorites.add(dish);
+    notifyListeners();
+  }
+
+  removeFavorite(Dish dish) {
+    favorites.remove(dish);
+    notifyListeners();
+  } */
+
+  addFavorite(Dish dish) async {
+    favorites.add(dish);
+    final Database db = await database();
+    await db.insert(
+      table,
+      {'name': dish.word.id},
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+     notifyListeners();
+  }
+
+  removeFavorite(Dish dish) async {
+    favorites.remove(dish);
+    final db = await database();
+    await db.delete(
+      table,
+      where: 'name = ?}',
+      // Pass the id as a whereArg to prevent SQL injection.
+      whereArgs: [dish.word.id],
+    );
+    notifyListeners();
+  }
+
+  _loadFavorites() async {
+    final Database db = await database();
+    final List<Map<String, dynamic>> maps = await db.query(table);
+    //dishes = maps.map((map) => DimSum.dishes[map['name']]).where((dish) => dish != null).toList();
   }
 }
