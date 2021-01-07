516b1a0caad4ff6e95a9681c5096089a9c61c48c
Philip Chu <atlassian@technicat.com>
2021-01-06 17:21:24 -0800

    Create favorite_dishes_widget.dart

---
+++ lib/ui/material/dishes/favorite_dishes_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:talkdimsum/core/provider/dimsum.dart';
+
+import 'dishes_widget.dart';
+
+class FavoriteDishesWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Favorites'),
+        ),
+        body: Consumer<DimSum>(builder: (context, dimsum, child) {
+          return DishesWidget(
+              dishes: dimsum.favorites);
+        }));
+  }
+}
