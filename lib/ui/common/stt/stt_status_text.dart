3e915d5708367c511ef4fe92bbcd8ea8441d1bf7
Philip Chu <atlassian@technicat.com>
2021-01-14 17:05:11 -0800

    Create stt_status_text.dart

---
+++ lib/ui/common/stt/stt_status_text.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/widgets.dart';
+
+import 'package:provider/provider.dart';
+
+import 'package:talkdimsum/core/provider/stt.dart';
+
+class STTStatusText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<STT>(builder: (context, stt, child) {
+      switch (stt.status) {
+        case STTStatus.Match:
+          {
+            return Text('Correct!',
+                textAlign: TextAlign.center, style: TextStyle(fontSize: 24));
+          }
+        case STTStatus.Mismatch:
+          {
+            return Text('Wrong!',
+                textAlign: TextAlign.center, style: TextStyle(fontSize: 24));
+          }
+        default:
+          {
+            return Text(stt.lastStatus,
+                textAlign: TextAlign.center, style: TextStyle(fontSize: 24));
+          }
+      }
+    });
+  }
+}
+
