cfd3d4615bd98a43cfa17c6bbf682534e2b1eec8
Philip Chu <atlassian@technicat.com>
2021-01-15 15:30:23 -0800

    places_list_view.dart

---
+++ lib/ui/cupertino/place/places_list_view.dart
@@ -0,0 +1,67 @@
+/* Technicat LLC */
+
+import 'package:flutter/cupertino.dart';
+
+import 'package:talkdimsum/core/model/place.dart';
+import 'package:talkdimsum/core/model/region.dart';
+
+import 'package:talkdimsum/ui/common/place/place_widget.dart';
+
+class PlacesListView extends StatelessWidget {
+  final Region region;
+
+  PlacesListView({Key key, @required this.region}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+                children: region.places
+                    .map((place) => PlaceRow(region: region, place: place))
+                    .toList());
+  }
+}
+
+class PlaceRow extends StatelessWidget {
+  final Region region;
+  final Place place;
+
+  PlaceRow({Key key, @required this.region, this.place}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var share = CupertinoActionSheetAction(
+        onPressed: () {
+          place.share();
+        },
+        child: Text('share'));
+    var map = CupertinoActionSheetAction(
+        onPressed: () {
+          place.showGoogleMap();
+        },
+        child: Text('map'));
+    var actions = [share, map];
+    actions.addAll(place.links
+        .map((link) => CupertinoActionSheetAction(
+            onPressed: () {
+              link.goto();
+            },
+            child: Text(link.name)))
+        .toList());
+    return CupertinoButton(
+        onPressed: () {
+          showCupertinoModalPopup(
+              context: context,
+              builder: (_) => CupertinoActionSheet(
+                  title: Text(place.name),
+                  message: Text(place.address),
+                  actions: actions
+                  /* cancelButton: CupertinoActionSheetAction(
+                        onPressed: () {
+                         // Navigator.pop(context);
+                        },
+                        child: Text('Cancel')*/
+                  ));
+        },
+        child: PlaceWidget(place: place));
+  }
+}
--- lib/ui/cupertino/place/region_scaffold.dart
+++ lib/ui/cupertino/place/region_scaffold.dart
@@ -2,10 +2,9 @@
 
 import 'package:flutter/cupertino.dart';
 
-import 'package:talkdimsum/core/model/place.dart';
 import 'package:talkdimsum/core/model/region.dart';
 
-import 'package:talkdimsum/ui/common/place/place_widget.dart';
+import 'places_list_view.dart';
 
 class RegionScaffold extends StatelessWidget {
   final Region region;
@@ -18,64 +17,5 @@ class RegionScaffold extends StatelessWidget {
         navigationBar: CupertinoNavigationBar(middle: Text(region.name)),
         child: SafeArea(
             child: PlacesListView(region: region)));
-  }
-}
-
-class PlacesListView extends StatelessWidget {
-  final Region region;
-
-  PlacesListView({Key key, @required this.region}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-                children: region.places
-                    .map((place) => PlaceRow(region: region, place: place))
-                    .toList());
-  }
-}
-
-class PlaceRow extends StatelessWidget {
-  final Region region;
-  final Place place;
-
-  PlaceRow({Key key, @required this.region, this.place}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var share = CupertinoActionSheetAction(
-        onPressed: () {
-          place.share();
-        },
-        child: Text('share'));
-    var map = CupertinoActionSheetAction(
-        onPressed: () {
-          place.showGoogleMap();
-        },
-        child: Text('map'));
-    var actions = [share, map];
-    actions.addAll(place.links
-        .map((link) => CupertinoActionSheetAction(
-            onPressed: () {
-              link.goto();
-            },
-            child: Text(link.name)))
-        .toList());
-    return CupertinoButton(
-        onPressed: () {
-          showCupertinoModalPopup(
-              context: context,
-              builder: (_) => CupertinoActionSheet(
-                  title: Text(place.name),
-                  message: Text(place.address),
-                  actions: actions
-                  /* cancelButton: CupertinoActionSheetAction(
-                        onPressed: () {
-                         // Navigator.pop(context);
-                        },
-                        child: Text('Cancel')*/
-                  ));
-        },
-        child: PlaceWidget(place: place));
   }
 }
