d402bec916df1b535f320840c8e439cb011790dd
Philip Chu <atlassian@technicat.com>
2021-01-21 08:19:42 -0800

    Update stt_status_text.dart

--- lib/ui/common/stt/stt_status_text.dart
+++ lib/ui/common/stt/stt_status_text.dart
@@ -14,6 +14,7 @@ class STTStatusText extends StatelessWidget {
             return Text('Listening...',
                 textAlign: TextAlign.center, style: TextStyle(fontSize: 24));
           }
+        case STTStatus.Match:
           {
             return Text('Correct!',
                 textAlign: TextAlign.center, style: TextStyle(fontSize: 24));
