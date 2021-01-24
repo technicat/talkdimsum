d0c2e6dbd938b15e76178c413da90040818f0f80
Philip Chu <atlassian@technicat.com>
2021-01-24 10:20:04 -0800

    removed talk_chinese for now

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,6 @@ import 'dart:io' show Platform;
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
-import 'package:talk_chinese/talk_chinese.dart';
-
 import 'package:talkdimsum/core/provider/dimsum.dart';
 import 'package:talkdimsum/core/provider/conversation.dart';
 import 'package:talkdimsum/core/provider/countries.dart';
