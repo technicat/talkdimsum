92770951952f3887b2018a2b7bdffd79c91bfe1b
Philip Chu <atlassian@technicat.com>
2021-02-07 21:02:45 -0800

    Update favorite_button.dart

--- lib/ui/cupertino/dish/favorite_button.dart
+++ lib/ui/cupertino/dish/favorite_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
-import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:talkdimsum/core/model/dish.dart';
 import 'package:talkdimsum/core/provider/dimsum.dart';
@@ -12,17 +12,25 @@ class FavoriteButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<DimSum>(
-        builder: (context, dimsum, child) => dimsum.isFavorite(dish)
-            ? CupertinoButton(
-                child: Icon(CupertinoIcons.heart_fill),
-                onPressed: () {
-                  dimsum.removeFavorite(dish);
-                })
-            : CupertinoButton(
-                child: Icon(CupertinoIcons.heart),
-                onPressed: () {
-                  dimsum.addFavorite(dish);
-                }));
+    return Consumer(builder: (context, watch, child) {
+      var dimsum = watch(dimsumProvider);
+      return dimsum.map(
+          data: (_) => _.value.isFavorite(dish)
+              ? CupertinoButton(
+                  child: Icon(CupertinoIcons.heart_fill),
+                  onPressed: () {
+                    _.value.removeFavorite(dish);
+                  })
+              : CupertinoButton(
+                  child: Icon(CupertinoIcons.heart),
+                  onPressed: () {
+                    _.value.addFavorite(dish);
+                  }),
+          loading: (_) => CupertinoActivityIndicator(),
+          error: (_) => Text(
+                _.error.toString(),
+                style: TextStyle(color: CupertinoColors.destructiveRed),
+              ));
+    });
   }
 }
