4993605242bcf7a55cf4e566a1bef5c6600c826e
Philip Chu <atlassian@technicat.com>
2021-02-22 20:26:46 -0800

    bubble tea

--- lib/ui/cupertino/router.dart
+++ lib/ui/cupertino/router.dart
@@ -5,13 +5,14 @@
 import 'package:auto_route/auto_route_annotations.dart';
 
 import 'navigation_bar.dart';
+import 'dishes/dishes_grid.dart';
 import 'dishes/word_dishes_scaffold.dart';
 
 @CupertinoAutoRouter(
-  //replaceInRouteName: 'Page,Route',
   routes: <AutoRoute>[
     AutoRoute(page: NavigationBar,initial:true),
     AutoRoute(page: WordDishesScaffold),
+    AutoRoute(page: DishesGrid),
   ],
 )
 class $AppRouter {}
--- lib/ui/cupertino/router.gr.dart
+++ lib/ui/cupertino/router.gr.dart
@@ -9,16 +9,20 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 
+import '../../core/model/dish.dart';
 import '../../core/model/word.dart';
+import 'dishes/dishes_grid.dart';
 import 'dishes/word_dishes_scaffold.dart';
 import 'navigation_bar.dart';
 
 class Routes {
   static const String navigationBar = '/';
   static const String wordDishesScaffold = '/word-dishes-scaffold';
+  static const String dishesGrid = '/dishes-grid';
   static const all = <String>{
     navigationBar,
     wordDishesScaffold,
+    dishesGrid,
   };
 }
 
@@ -28,6 +32,7 @@ class AppRouter extends RouterBase {
   final _routes = <RouteDef>[
     RouteDef(Routes.navigationBar, page: NavigationBar),
     RouteDef(Routes.wordDishesScaffold, page: WordDishesScaffold),
+    RouteDef(Routes.dishesGrid, page: DishesGrid),
   ];
   @override
   Map<Type, AutoRouteFactory> get pagesMap => _pagesMap;
@@ -48,6 +53,16 @@ class AppRouter extends RouterBase {
         settings: data,
       );
     },
+    DishesGrid: (data) {
+      final args = data.getArgs<DishesGridArguments>(nullOk: false);
+      return CupertinoPageRoute<dynamic>(
+        builder: (context) => DishesGrid(
+          key: args.key,
+          dishes: args.dishes,
+        ),
+        settings: data,
+      );
+    },
   };
 }
 
@@ -60,4 +75,11 @@ class WordDishesScaffoldArguments {
   final Key key;
   final Word word;
   WordDishesScaffoldArguments({this.key, @required this.word});
+}
+
+/// DishesGrid arguments holder class
+class DishesGridArguments {
+  final Key key;
+  final List<Dish> dishes;
+  DishesGridArguments({this.key, @required this.dishes});
 }
