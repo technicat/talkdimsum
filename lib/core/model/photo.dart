967980cdb15b8eae4daffa1f2f4bc14c1ff1e8f7
Philip Chu <atlassian@technicat.com>
2021-02-27 15:07:04 -0800

    json

--- lib/core/model/photo.dart
+++ lib/core/model/photo.dart
@@ -1,10 +1,17 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'photo.g.dart';
+
+@JsonSerializable()
 class Photo {
+  @JsonKey(name: 'image')
   final String name;
   final String place;
 
   Photo(this.name, this.place);
 
-  Photo.fromJson(Map<String, dynamic> json)
-      : name = json['image'],
-        place = json['place'];
+  factory Photo.fromJson(Map<String, dynamic> json) => _$PhotoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PhotoToJson(this);
+
 }
---
+++ lib/core/model/photo.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'photo.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Photo _$PhotoFromJson(Map<String, dynamic> json) {
+  return Photo(
+    json['image'] as String,
+    json['place'] as String,
+  );
+}
+
+Map<String, dynamic> _$PhotoToJson(Photo instance) => <String, dynamic>{
+      'image': instance.name,
+      'place': instance.place,
+    };
