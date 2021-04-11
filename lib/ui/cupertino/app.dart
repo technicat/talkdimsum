41babf85799d54c92c7cf06cd2e3a3b1dde6c38a
Philip Chu <atlassian@technicat.com>
2021-04-10 20:21:52 -0700

    Update app.dart

--- lib/ui/cupertino/app.dart
+++ lib/ui/cupertino/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+//import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_messaging/firebase_messaging.dart';
 
 
 import 'navigation_bar.dart';
