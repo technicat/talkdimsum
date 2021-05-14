94fbd532aa73ce2229d92cb415a39ce58fb47ee0
Philip Chu <atlassian@technicat.com>
2021-05-14 15:50:52 -0700

    Update region.dart

--- lib/core/model/region.dart
+++ lib/core/model/region.dart
@@ -6,6 +6,8 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'place.dart';
 
 import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
 
 part 'region.g.dart';
 
