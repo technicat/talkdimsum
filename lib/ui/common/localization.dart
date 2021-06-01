d2fef9f3aba6b14eceed62ddc30a5e7eff82ad47
Philip Chu <atlassian@technicat.com>
2021-05-31 22:23:30 -0700

    Update localization.dart

--- lib/ui/common/localization.dart
+++ lib/ui/common/localization.dart
@@ -3,13 +3,29 @@ import 'package:get/get.dart';
 class Localization extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
+        'de_DE': {
+          'title': 'Dim Sum Reden',
+          'search': 'suchen',
+        },
         'en_US': {
           'title': 'Talk Dim Sum',
           'search': 'Search',
         },
-        'de_DE': {
-          'title': 'Dim Sum Reden',
-            'search': 'Suche',
-        }
+        'es_MX': {
+          'title': 'Hablar Dim Sum',
+          'search': 'buscar',
+        },
+        'fr_FR': {
+          'title': 'Parler Dim Sum',
+          'search': 'recherche',
+        },
+        'it_IT': {
+          'title': 'Parlere Dim Sum',
+          'search': 'recirca',
+        },
+        'pt_PT': {
+          'title': 'Falar Dim Sum',
+          'search': 'buscar',
+        },
       };
 }
