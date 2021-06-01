1e058c53bf4f99a04460996959031de6529a8ea1
Philip Chu <atlassian@technicat.com>
2021-06-01 15:21:19 -0700

    Update play_button.dart

--- lib/ui/cupertino/tts/play_button.dart
+++ lib/ui/cupertino/tts/play_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-
 import 'package:provider/provider.dart';
 
 import 'package:talkdimsum/core/model/word.dart';
@@ -12,15 +11,12 @@ class PlayButton extends StatelessWidget {
   PlayButton({Key? key, required this.word}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Consumer<Settings>(builder: (context, settings, child) {
-      return Consumer<TTS>(
-          builder: (context, tts, child) => CupertinoButton(
-                onPressed: () =>
-                    tts.say(word, settings.language, settings.speed),
-                child: Icon(CupertinoIcons.volume_up,
-                    semanticLabel: 'Pronounce this word'),
-              ));
-    });
-  }
+  Widget build(BuildContext context) => Consumer<Settings>(
+        builder: (context, settings, child) => Consumer<TTS>(
+            builder: (context, tts, child) => CupertinoButton(
+                  onPressed: () =>
+                      tts.say(word, settings.language, settings.speed),
+                  child: Icon(CupertinoIcons.volume_up,
+                      semanticLabel: 'Pronounce this word'),
+                )));
 }
