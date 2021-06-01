2b782b9e8391a8df507b809132944f3ee08bcc87
Philip Chu <atlassian@technicat.com>
2021-06-01 16:39:32 -0700

    Update tts.dart

--- lib/core/provider/tts.dart
+++ lib/core/provider/tts.dart
@@ -7,20 +7,23 @@ import 'package:talkdimsum/core/model/language.dart';
 import 'package:talkdimsum/core/model/word.dart';
 
 class TTS with ChangeNotifier {
-
   FlutterTts flutterTts = FlutterTts();
 
+  String uttering = '';
+
   Future _say(String text, Chinese lang, double speed) async {
-    try {
-      // Platform check fails on web
-      if (GetPlatform.isIOS || GetPlatform.isMacOS) {
-        await flutterTts
-            .setSpeechRate(speed * 0.35); // tts plays back fast on ios
-      } else {
-        await flutterTts.setSpeechRate(speed * 0.8);
-      }
-    } catch (e) {
-      await flutterTts.setSpeechRate(speed);
+    if (GetPlatform.isIOS || GetPlatform.isMacOS) {
+      await flutterTts
+          .setSpeechRate(speed * 0.35); // tts plays back fast on ios
+    } else {
+      await flutterTts.setSpeechRate(speed * 0.8);
+    }
+    if (flutterTts.progressHandler == null) {
+      flutterTts.setProgressHandler(
+          (String text, int start, int end, String word) {
+            print(word);
+            uttering = word;
+          });
     }
     // language change not working on iOS
     // need to call setVoice?
@@ -85,5 +88,5 @@ flutterTts.setContinueHandler((msg) {
   });
 });
 */
-  
+
 }
