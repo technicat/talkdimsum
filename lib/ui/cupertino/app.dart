9ad3352e9bf43eb18aa8a5d153c120a8b9bb5aa4
Philip Chu <atlassian@technicat.com>
2021-06-05 08:34:50 -0700

    pt

--- lib/ui/cupertino/app.dart
+++ lib/ui/cupertino/app.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_localizations/flutter_localizations.dart';
+//import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/cupertino.dart';
 
