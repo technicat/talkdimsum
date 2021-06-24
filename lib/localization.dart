0ac420391baa0e0a043c3ad388a17efb5b88068a
Philip Chu <atlassian@technicat.com>
2021-06-24 11:06:18 -0700

    try flutter_sheet_localization

---
+++ lib/localization.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_sheet_localization/flutter_sheet_localization.dart';
+
+part 'localization.g.dart';
+
+@SheetLocalization('1YfO00hGOWH4mFy5yuRXAxZ1-4WEMESyN26Sz0aWJkg8', 
+'0', 1) // <- See 1. to get DOCID and SHEETID
+// the `1` is the generated version. You must increment it each time you want to regenerate
+// a new version of the labels.
+class AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizationsData> {
+  const AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => localizedLabels.containsKey(locale);
+
+  @override
+  Future<AppLocalizationsData> load(Locale locale) =>
+      SynchronousFuture<AppLocalizationsData>(localizedLabels[locale]!);
+  @override
+  bool shouldReload(AppLocalizationsDelegate old) => false;
+}
