70f9551e45121527132bca47a786bc184fa548eb
Philip Chu <atlassian@technicat.com>
2021-07-20 22:07:27 -0700

    rm substring_highlight

--- lib/ui/material/word/chinese_text.dart
+++ lib/ui/material/word/chinese_text.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 
-import 'package:substring_highlight/substring_highlight.dart';
-
 import 'package:talkdimsum/core/model/word.dart';
 import 'package:talkdimsum/core/provider/settings.dart';
 
@@ -18,23 +16,5 @@ class ChineseText extends StatelessWidget {
         builder: (context, settings, child) => ExcludeSemantics(
             child: SelectableText(word.chineseText(settings.language),
                 textAlign: TextAlign.center, style: TextStyle(fontSize: 24))));
-  }
-}
-
-class HighlightChineseText extends StatelessWidget {
-  final Word word;
-
-  HighlightChineseText({Key? key, required this.word}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<Settings>(
-        builder: (context, settings, child) => Directionality(
-            textDirection: TextDirection.ltr,
-            child: SubstringHighlight(
-                text: '${word.chineseText(settings.language)}',
-                term: '',
-                textAlign: TextAlign.center,
-                textStyle: TextStyle(fontSize: 24))));
   }
 }
