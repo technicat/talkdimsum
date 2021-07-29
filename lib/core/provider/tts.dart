7960bf85cdde4b825bd745031eb7cd3b07f9026f
Phil Chu <technicat@users.noreply.github.com>
2021-07-28 17:10:53 -0700

    adjusted tts rate

--- lib/core/provider/tts.dart
+++ lib/core/provider/tts.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart' show ChangeNotifier;
 
 import 'package:flutter_tts/flutter_tts.dart';
-import 'package:get/get.dart';
 
 import 'package:talkdimsum/core/model/language.dart';
 import 'package:talkdimsum/core/model/word.dart';
@@ -12,25 +11,15 @@ class TTS with ChangeNotifier {
   String uttering = '';
 
   Future _say(String text, Chinese lang, double speed) async {
-    if (GetPlatform.isIOS || GetPlatform.isMacOS) {
-      await flutterTts
-          .setSpeechRate(speed * 0.35); // tts plays back fast on ios
-    } else {
-      await flutterTts.setSpeechRate(speed * 0.8);
-    }
+    await flutterTts.setSpeechRate(speed * 0.5);
     if (flutterTts.progressHandler == null) {
-      flutterTts.setProgressHandler(
-          (String text, int start, int end, String word) {
-            uttering = word;
-          });
+      flutterTts
+          .setProgressHandler((String text, int start, int end, String word) {
+        uttering = word;
+      });
     }
-    // language change not always working on iOS
-    // need to call setVoice?
-    // List<dynamic> languages = await flutterTts.getLanguages;
-    // print(languages);
     var loc = Language.locale(lang);
     await flutterTts.setLanguage(loc);
-    //await flutterTts.setVoice({"locale": loc});
     var result = await flutterTts.speak(text);
     if (result == 1) {
       print('speaking in ' + loc);
@@ -42,5 +31,4 @@ class TTS with ChangeNotifier {
   Future say(Word word, Chinese lang, double speed) async {
     await _say(word.chineseText(lang), lang, speed);
   }
-
 }
