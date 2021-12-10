b3265faa82f5c8387b9ce4b88631e5d1d6a95561
Philip Chu <atlassian@technicat.com>
2021-12-10 14:41:36 -0800

    Update dish_widget.dart

--- lib/ui/cupertino/dish/dish_widget.dart
+++ lib/ui/cupertino/dish/dish_widget.dart
@@ -6,8 +6,8 @@ import 'package:talkdimsum/ui/common/dish/dish_tap_image_widget.dart';
 import 'package:talkdimsum/ui/cupertino/word/word_widget.dart';
 
 // todo - cupertino version
-import 'package:talkdimsum/ui/material/dish/dish_character_menu_button.dart';
-import 'package:talkdimsum/ui/material/dish/dish_info_menu_button.dart';
+//import 'package:talkdimsum/ui/material/dish/dish_character_menu_button.dart';
+//import 'package:talkdimsum/ui/material/dish/dish_info_menu_button.dart';
 
 import 'favorite_button.dart';
 
@@ -22,11 +22,37 @@ class DishWidget extends StatelessWidget {
         WordWidget(word: dish.word),
         Expanded(child: DishDescriptionWidget(dish: dish)),
         Row(children: [
-        //  DishInfoMenuButton(dish: dish),
+          DishInfoButton(dish: dish),
           Spacer(),
           FavoriteButton(dish: dish),
           Spacer(),
-      //    DishCharacterMenuButton(dish: dish),
+          //    DishCharacterMenuButton(dish: dish),
         ])
       ]);
+}
+
+class DishInfoButton extends StatelessWidget {
+  final Dish dish;
+
+  DishInfoButton({Key? key, required this.dish}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+        onPressed: () {
+          showCupertinoModalPopup(
+              context: context,
+              builder: (_) => CupertinoActionSheet(
+                      title: Text('More about...'),
+                      message: Text(dish.word.english),
+                      actions: [
+                        ...dish.resources.map((link) =>
+                            CupertinoActionSheetAction(
+                                onPressed: () => link.goto(),
+                                child: Text(link.name)))
+                      ]));
+        },
+        child:
+            Icon(CupertinoIcons.info, semanticLabel: 'Links to web resources'));
+  }
 }
