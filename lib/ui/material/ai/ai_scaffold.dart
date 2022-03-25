eb140f350faa402b1b47e2b491337cb313fb7dea
Philip Chu <atlassian@technicat.com>
2022-03-24 19:30:17 -0700

    ai

--- lib/ui/material/ai/ai_scaffold.dart
+++ lib/ui/material/ai/ai_scaffold.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import 'categories_grid.dart';
+import '../dishes/categories_grid.dart';
 
 class AIScaffold extends StatelessWidget {
   @override
