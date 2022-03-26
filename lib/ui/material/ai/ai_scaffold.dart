be57e8af6f90808490047c8711743c38487ee37a
Philip Chu <atlassian@technicat.com>
2022-03-26 00:40:29 -0700

    Update ai_scaffold.dart

--- lib/ui/material/ai/ai_scaffold.dart
+++ lib/ui/material/ai/ai_scaffold.dart
@@ -3,6 +3,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import 'dart:async';
+import 'package:camera/camera.dart';
+
 class AIScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -13,4 +16,52 @@ class AIScaffold extends StatelessWidget {
             ]),
       
       );
+}
+
+/*
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+  runApp(CameraApp());
+}
+*/
+
+class CameraApp extends StatefulWidget {
+  @override
+  _CameraAppState createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  CameraController? controller;
+
+List<CameraDescription> cameras = [];
+  @override
+  void initState() async {
+    super.initState();
+    cameras = await availableCameras();
+    controller = CameraController(cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return MaterialApp(
+      home: CameraPreview(controller),
+    );
+  }
 }
