224bf22d872819bc55cba17af375488540d86038
Philip Chu <atlassian@technicat.com>
2022-04-20 10:34:04 -0700

    cleanup

--- lib/ui/cupertino/place/places_list_view.dart
+++ lib/ui/cupertino/place/places_list_view.dart
@@ -1,11 +1,9 @@
 /* Technicat LLC */
 
-import 'package:flutter/cupertino.dart';
-
-import 'package:talkdimsum/core/model/place.dart';
+import 'package:flutter/widgets.dart';
 import 'package:talkdimsum/core/model/region.dart';
 
-import 'package:talkdimsum/ui/common/place/place_widget.dart';
+import 'place_row.dart';
 
 class PlacesListView extends StatelessWidget {
   final Region region;
@@ -17,36 +15,4 @@ class PlacesListView extends StatelessWidget {
       children: region.places
           .map((place) => PlaceRow(region: region, place: place))
           .toList());
-}
-
-class PlaceRow extends StatelessWidget {
-  final Region region;
-  final Place place;
-
-  PlaceRow({Key? key, required this.region, required this.place})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var share = CupertinoActionSheetAction(
-        onPressed: () => place.share(), child: Text('share'));
-    var map = CupertinoActionSheetAction(
-        onPressed: () => place.showGoogleMap(), child: Text('map'));
-    return CupertinoButton(
-        onPressed: () {
-          showCupertinoModalPopup(
-              context: context,
-              builder: (_) => CupertinoActionSheet(
-                      title: Text(place.name),
-                      message: Text(place.address),
-                      actions: [
-                        share,
-                        map,
-                        ...place.links.map((link) => CupertinoActionSheetAction(
-                            onPressed: () => link.goto(),
-                            child: Text(link.name)))
-                      ]));
-        },
-        child: PlaceWidget(place: place));
-  }
 }
