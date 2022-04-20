cd03a3213ae4751c940c044171d52b15fbc5bf93
Philip Chu <atlassian@technicat.com>
2022-04-20 11:09:38 -0700

    markdown links work

--- lib/ui/common/dish/dish_description_widget.dart
+++ lib/ui/common/dish/dish_description_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/widgets.dart';
-
-import 'package:flutter_markdown/flutter_markdown.dart'; // as md;
-
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:talkdimsum/core/model/dish.dart';
 
 class DishDescriptionWidget extends StatelessWidget {
@@ -10,5 +9,11 @@ class DishDescriptionWidget extends StatelessWidget {
   DishDescriptionWidget({Key? key, required this.dish}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Markdown(data: dish.description);
+  Widget build(BuildContext context) => Markdown(
+      data: dish.description,
+      onTapLink: (text, url, title) {
+        if (url != null) {
+          launch(url);
+        }
+      });
 }
