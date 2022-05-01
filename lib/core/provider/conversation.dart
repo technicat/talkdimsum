a047083fd0aca1c242f7a865610965a062c40045
Philip Chu <technicat@users.noreply.github.com>
2022-04-30 23:29:04 -0700

    Update conversation.dart

--- lib/core/provider/conversation.dart
+++ lib/core/provider/conversation.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
-
 import 'package:flutter/foundation.dart' show ChangeNotifier;
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:talkdimsum/core/model/phrases.dart';
