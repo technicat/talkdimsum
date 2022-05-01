13e07b06a442d7fb5f683ddf999ffad6c47b269b
Philip Chu <technicat@users.noreply.github.com>
2022-04-30 23:28:45 -0700

    Update phrases_test.dart

--- test/phrases_test.dart
+++ test/phrases_test.dart
@@ -4,7 +4,7 @@ import 'package:talkdimsum/core/provider/conversation.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  test('loading countries', () async {
+  test('loading phrases', () async {
     var con = Conversation();
     await con.load();
     expect(con.phrases.isNotEmpty, true);
