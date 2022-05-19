cd6049fa33aac88187b7487c517b388d13d66331
Philip Chu <technicat@users.noreply.github.com>
2022-05-19 14:17:34 -0700

    use material 3

--- lib/ui/material/app.dart
+++ lib/ui/material/app.dart
@@ -11,6 +11,7 @@ import 'navigationbar.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) => GetMaterialApp(
+        theme: ThemeData.light().copyWith(useMaterial3: true),
         home: MyNavigationBar(),
         onGenerateTitle: (BuildContext context) => 'Talk Dim Sum',
       );
