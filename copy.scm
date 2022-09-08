821bf4e081dd0006e3244108be71b26f65cf2e4d
Philip Chu <technicat@users.noreply.github.com>
2022-09-07 23:08:37 -0700

    Update copy.scm

--- copy.scm
+++ copy.scm
@@ -12,4 +12,8 @@
 
 (copy-directory* "assets/json/place" "../swiftdimsum/Fugu Dim Sum/places" :if-exists :supersede)
 
-(copy-directory* "assets/json/resources/" "../swiftdimsum/Fugu Dim Sum/resources" :if-exists :supersede)
+(copy-directory* "assets/json/resources" "../swiftdimsum/Fugu Dim Sum/resources" :if-exists :supersede)
+
+(copy-directory* "assets/json/words" "../swiftdimsum/Fugu Dim Sum/words" :if-exists :supersede)
+
+(copy-directory* "assets/json/phrases" "../swiftdimsum/Fugu Dim Sum/phrases" :if-exists :supersede)
