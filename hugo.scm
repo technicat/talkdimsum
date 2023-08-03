a26bd60df99a9fc838a388d5fe6f6a63f9c37d3a
Philip Chu <technicat@users.noreply.github.com>
2023-08-03 01:12:12 -0600

    Update hugo.scm

--- hugo.scm
+++ hugo.scm
@@ -17,17 +17,18 @@
    (v "v|verbose")
    . restargs)
   (if (not h)
-   (let ((cats (read-json "assets/json/dish/categories.json"))
-         (dishes (read-dishes)))
-    (if v (print cats))
+   (let ((dishes (read-dishes)))
     (if v (print dishes))))))
 
 (define (help file)
  (print "dimsum.scm -f file -o outfile -v -h"))
 
 
-(define read-dishes
+(define (read-dishes)
  (let ((files (read-json "assets/json/dish/dishes.json")))
   (fold 'concat (map (lambda (file)
                       (read-json #"assets/json/dish/~|file|.json"))
                  files) [])))
+
+(define (read-cats)
+ (let ((words (read-json "assets/json/dish/categories.json")))))
